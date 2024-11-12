7a68168cb6465ada56596677d1995be4a905d1b7
xava-44 <jsmith.jvs13@gmail.com>
2024-11-11 20:51:25 -0800

    se agrego nuevos metodos ala estructura no lineal

--- EDDProy/Estructuras No Lineales/frmArboles.cs
+++ EDDProy/Estructuras No Lineales/frmArboles.cs
@@ -20,14 +20,14 @@ using System.Xml.Linq;
 //using csdot.Attributes.DataTypes;
 
 namespace EDDemo.Estructuras_No_Lineales
-{  
+{
     // la clase frm(fomulario)Arboles , esta se encarga de concectar los conponectes  
     // del formulario con los metodos y atributos
     public partial class frmArboles : Form
 
     {   // se crea un atributo de tipo ArbolBusquda es un la clase que contiene todo los metodos de un arbol
         ArbolBusqueda miArbol;
-        
+
         // este atributo es de tipo NodoBinario 
         // este objeto contiene sus propios atributos 
         NodoBinario miRaiz;
@@ -38,15 +38,15 @@ namespace EDDemo.Estructuras_No_Lineales
             miArbol = new ArbolBusqueda();
             miRaiz = null;
         }
-    // componente boton para agregar un nodo al arbol
+        // componente boton para agregar un nodo al arbol
         private void btnAgregar_Click(object sender, EventArgs e)
         {
             try
-            {       //Obtenemos el nodo Raiz del arbol
-                     miRaiz = miArbol.RegresaRaiz();
-
+            {      //Obtenemos el nodo Raiz del arbol
+                miRaiz = miArbol.RegresaRaiz();
+                // Obtenemos el dado de la caja de texto
                 int dato = int.Parse(txtDato.Text);
-
+                // buscamos el dato dentro del arbol y comparamos si se encuentra en el 
                 if (miArbol.Busqueda(dato, miRaiz) == true)
                 {
 
@@ -54,7 +54,7 @@ namespace EDDemo.Estructuras_No_Lineales
                     return;
                 }
 
-               
+
 
                 //Limpiamos la cadena donde se concatenan los nodos del arbol 
                 miArbol.strArbol = "";
@@ -67,27 +67,45 @@ namespace EDDemo.Estructuras_No_Lineales
                 txtArbol.Text = miArbol.strArbol;
 
                 txtDato.Text = "";
+
+
+
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 MessageBox.Show("no se a ingresado un dato");
             }
 
- 
-        }
 
+        }
+        // componente boton para podar un  arbol
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
-            miArbol = null;
-            miRaiz = null;
-            miArbol = new ArbolBusqueda();
-            txtArbol.Text  = "";
-            txtDato.Text = "";
+
+            if (miRaiz == null)
+            {
+                MessageBox.Show("arbol vacio");
+            }
+            // Obtenemos la raiz
+            miRaiz = miArbol.RegresaRaiz();
+            // llamamos al metodo podar 
+            miArbol.PodarArbol(ref miRaiz);
+           
+            //miArbol = new ArbolBusqueda();
+            // limpiamos la etiquetas y los textbox 
+            txtArbol.Clear();
+            txtDato.Clear();
             lblRecorridoPreOrden.Text = "";
             lblRecorridoInOrden.Text = "";
             lblRecorridoPostOrden.Text = "";
+            labelAltura.Text = "";
+            labelamplitud.Text = "";
+            lablleno.Text = "";
+            labnodos.Text = "";
+            labcantidahojas.Text = "";
+            labcompleto.Text = "";
         }
-
+        // componente boton para graficar un arbol
         private void btnGrafica_Click(object sender, EventArgs e)
         {
             String graphVizString;
@@ -103,20 +121,21 @@ namespace EDDemo.Estructuras_No_Lineales
             b.Append("digraph G { node [shape=\"circle\"]; " + Environment.NewLine);
             b.Append(miArbol.ToDot(miRaiz));
             b.Append("}");
-            graphVizString =  b.ToString();
+            graphVizString = b.ToString();
 
             //graphVizString = @" digraph g{ label=""Graph""; labelloc=top;labeljust=left;}";
             //graphVizString = @"digraph Arbol{Raiz->60; 60->40. 60->90; 40->34; 40->50;}";
             Bitmap bm = FileDotEngine.Run(graphVizString);
 
-            
+
             frmGrafica graf = new frmGrafica();
             graf.ActualizaGrafica(bm);
             graf.MdiParent = this.MdiParent;
             graf.Show();
         }
 
-
+        // componente boton para recorrer un arbol de diferentes formas
+        // inorden , preorden , inorden , por niveles 
         private void btnRecorrer_Click(object sender, EventArgs e)
         {
             //Recorrido en PreOrden
@@ -155,15 +174,23 @@ namespace EDDemo.Estructuras_No_Lineales
             miRaiz = miArbol.RegresaRaiz();
             miArbol.strRecorrido = "";
 
-            if (miRaiz == null) {
+            if (miRaiz == null)
+            {
                 lblRecorridoPostOrden.Text = "El arbol esta vacio";
                 return;
             }
-            lblRecorridoPostOrden.Text = ""; 
+            lblRecorridoPostOrden.Text = "";
             miArbol.PostOrden(miRaiz);
             lblRecorridoPostOrden.Text = miArbol.strRecorrido;
+
+
+           // Recorrido por niveles 
+            miArbol.RecorridoNiveles(miRaiz ,labelamplitud);
+         
+           
         }
 
+        // componente boton para crear un arbol 
         private void btnCrearArbol_Click(object sender, EventArgs e)
         {
             //Limpiamos los objetos y clases del anterior arbol
@@ -172,12 +199,12 @@ namespace EDDemo.Estructuras_No_Lineales
             miArbol = new ArbolBusqueda();
             txtArbol.Text = "";
             txtDato.Text = "";
-             
+
             miArbol.strArbol = "";
 
             Random rnd = new Random();
 
-            for (int nNodos =1; nNodos < txtNodos.Value; nNodos++)
+            for (int nNodos = 1; nNodos < txtNodos.Value; nNodos++)
             {
                 int Dato = rnd.Next(1, 100);
                 //Obtenemos el nodo Raiz del arbol
@@ -192,10 +219,47 @@ namespace EDDemo.Estructuras_No_Lineales
             txtArbol.Text = miArbol.strArbol;
 
             txtDato.Text = "";
+
+            // Obtenemos la raiz
+            miRaiz = miArbol.RegresaRaiz();
+            // Se llama al metodo que regresa la altura del arbol
+           int altura = miArbol.Altura(miRaiz);
+            // se convierte a cadena y se imprime en una etiqueta 
+            labelAltura.Text = altura.ToString();
+
+            //se limpia la etiqueta
+            labcantidahojas.Text = "";
+            // se llama que cuenta las hojas del arbol 
+            int hojas = miArbol.Contarhojas(miRaiz);
+            // se convierte a cadena y se imprime en una etiqueta 
+            labcantidahojas.Text = hojas.ToString();
+
+
+            //se limpia la etiqueta
+            labnodos.Text = "";
+            // se llama a al metodo contador de nodos de un arbol
+            int nodos =miArbol.Contarnodos(miRaiz);
+            // se convierte a cadena y se imprime en una etiqueta 
+            labnodos.Text= nodos.ToString();
+
+            // se limpia la etiqueta 
+            lablleno.Text = "";
+            // se llama al metodo que revisa si es un arbol lleno 
+            bool lleno=miArbol.EsLleno(miRaiz);
+             // se convierte a cadena y se imprime en una etiqueta
+            lablleno.Text= lleno.ToString();
+
+            // este metodo regresa un booleano y revisa si el arbol es completo 
+            bool completo=miArbol.EsCompleto(miRaiz);
+            // se convierte a cadena y se imprime en una etiqueta 
+            labcompleto.Text= completo.ToString();
+
+
+
         }
 
-        
 
+        // componente boton para buscar un nodo en arbol binario 
         private void botonbuscar_Click(object sender, EventArgs e)
         {
             try
@@ -218,12 +282,58 @@ namespace EDDemo.Estructuras_No_Lineales
                 }
 
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 MessageBox.Show("ingrese un dato!!!");
             }
 
         }
+        // componente boton sustituir un nodo por su predecesor
+        private void butonpredecesor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                miRaiz = miArbol.RegresaRaiz();
+                int num = int.Parse(cajaprede.Text);
+                miArbol.EliminarPredecesor(num, ref miRaiz);
 
+
+
+
+                miArbol.MuestraArbolAcostado(1, miRaiz);
+                txtArbol.Text = miArbol.strArbol;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ingrese un dato");
+            }
+
+
+        }
+        // componente boton sustituir un nodo por su sucesor
+        private void butonsucesor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                miRaiz = miArbol.RegresaRaiz();
+
+                int num = int.Parse(cajasucesor.Text);
+
+
+                miArbol.EliminarSucesor(num, ref miRaiz);
+
+
+
+                miArbol.MuestraArbolAcostado(1, miRaiz);
+                txtArbol.Text = miArbol.strArbol;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ingrese un dato");
+            }
+
+
+        }
 
         private void frmArboles_Load(object sender, EventArgs e)
         {
@@ -232,7 +342,19 @@ namespace EDDemo.Estructuras_No_Lineales
 
         private void txtArbol_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
+
+        private void lblRecorridoPostOrden_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
---
+++ EDDProy/Estructuras lineales/formcolas1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDDemo
+{
+    public partial class formcolas1 : Form
+    {  public static Nodo inicio = null;
+        public static Nodo final = null;
+        public List<int> BusquedaList;
+
+        public formcolas1()
+        {
+            InitializeComponent();
+        }
+      
+       
+      
+        public class Nodo
+        {
+            public int valor;
+            public Nodo sig;
+
+            public Nodo(int valor, Nodo sig)
+            {
+                this.valor = valor;
+                this.sig = sig;
+            }
+
+
+
+        }
+    }
+}
